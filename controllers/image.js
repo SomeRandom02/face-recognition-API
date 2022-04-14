16b95adf534044d355a45c4b62bebdf0816b9b4b
SomeRandom02 <hoopercolin342@gmail.com>
2022-04-14 06:47:34 -0400

    fix

--- controllers/image.js
+++ controllers/image.js
@@ -42,10 +42,10 @@ const handleApiCall = (req, res) => {
 const handleImage = (req, res, db) => {
     const { id } = req.body;
     db('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-        res.json(entries[0].entries);
+    .increment('entires', 1)
+    .returning('entires')
+    .then(entires => {
+        res.json(entires[0].entires);
     })
     .catch(err => res.status(400).json('unable to get entries'))
 }
