6585418d46319fee9a7031b06004c0486fb92654
SomeRandom02 <hoopercolin342@gmail.com>
2022-04-14 05:59:45 -0400

    fix

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const image = require('./controllers/image');
 
 const db = knex({
     client: 'pg',
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
+    connection: {
+        host : process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
